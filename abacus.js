f4cd1d5c038b101dd0b11c71221a5fda6dcbca17
jjaspe <jjaspe09@hotmail.com>
2014-11-07 22:12:11 -0500

    Added id function in abacus.js
    Added function to put ids on all divs containing images, and added tests
    to every step of the process.

--- abacus.js
+++ abacus.js
@@ -1,16 +1,59 @@
-var moveAbacusParts=function(){
-	var jumbotronTop=$('.jumbotron').top + "px";
+  var testUpperFirstIdIsZero = function(el)
+  {
+   	if (el.id==="0")
+  	   console.log( "First Id Test passes");
+  	else
+  		console.log( "First ID Test fails. " + $(el).attr('id'));
+  }
+  
+  var testUpperLastIdIsZero = function(el)
+  {
+   	if (el.id==="11")
+  	   console.log( "Last Id Test passes");
+  	else
+  		console.log( "Last ID Test fails. " + $(el).attr('id'));
+  }
+  
+  var testImagesMoreThanZero = function(el)
+  {
+   	if (el.length>0)
+  	   console.log( "Images Test passes " + el.length);
+  	else
+  		console.log( "Images Test fails. " );
+  }
+  
+  var testRowsMoreThanZero = function(el)
+  {
+   	if (el.length>0)
+  	   console.log( "Rows Test passes " + el.length);
+  	else
+  		console.log( "Rows Test fails. " );
+  }
+  
+
+/* Gives ids to all divs containing images so we identify them */
+var setUpIds=function()
+{
+ 	//This one has only one row,so just get all divs from that;
+	var row=$('.upper').children('div');
+ 	var images=$(row).children('div');
 	
-	$('.upper').css(
+	$.each(images,function(index,value)
 	{
-	 position:'absolute',
-	 top:jumbotronTop
+	 $(value).attr("id",index);;
 	 });
-};
+}
 
 var main=function(){
-	//moveAbacusParts();
+	var row=$('.upper').children('div');
+	testRowsMoreThanZero(row);
 	
+ 	var images=$(row).children('div');
+	testImagesMoreThanZero(images);
+	
+	setUpIds();	
+	testUpperFirstIdIsZero(images[0]);
+	testUpperLastIdIsZero(images[11]);
 }
 	
 $(document).ready(main);
